e196bf0baed070f0fd2265a936a09adc54fad21d
fklid <fklid1972@gmail.com>
2026-04-21 08:47:26 +0300

    Добавил класс в TestPetShopDS проверку на PUTtestGetNonexistentPet

--- src/test/java/tests/TestPetShopDS.java
+++ src/test/java/tests/TestPetShopDS.java
@@ -78,4 +78,31 @@ public class TestPetShopDS {
         );
 
     }
+
+    @Test
+    @Feature("Pet")
+    @Severity(SeverityLevel.CRITICAL)
+    @Owner("dmitry shkolnik")
+    public void testGetNonexistentPet() {
+        Response response = step("Отправить GET запрос для получения информации о несуществующем питомце", () ->
+
+                given()
+                        .contentType(ContentType.JSON)
+                        .header("Accept", "application/json")
+                        .when()
+                        .get(BASE_URL + "/pet/9999"));
+
+        String responseBody = response.getBody().asString();
+
+        step("Проверить, что стату-скод ответа ==404", () ->
+
+                assertEquals(404, response.getStatusCode(),
+                        "Код ответа не совпал с ожидаемым. Ответ: " + responseBody)
+        );
+        step("Проверить, что стату-скод ответа ==404", () ->
+
+                assertEquals("Pet not found", responseBody,
+                        "Текст ошибки не совпал с ожидаемым. Получен: " + responseBody)
+        );
+    }
 }
